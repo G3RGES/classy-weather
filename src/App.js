fca8c667f4493a851c21996f66a8e9562b0958af
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-21 03:55:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,39 +2,10 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>Hello, Classy Weather!</h1>
+      </div>
+    );
   }
 }
-
-// export default class Counter extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { count: 0 };
-//     this.handleDecrement = this.handleDecrement.bind(this);
-//     this.handleIncrement = this.handleIncrement.bind(this);
-//   }
-
-//   handleDecrement = () => {
-//     this.setState((prevState) => ({ count: prevState.count - 1 }));
-//   };
-//   handleIncrement = () => {
-//     this.setState((prevState) => ({ count: prevState.count + 1 }));
-//   };
-
-//   render() {
-//     const date = new Date("2024-06-01T12:00:00");
-//     date.setDate(date.getDate() + this.state.count);
-
-//     return (
-//       <div>
-//         <h1>Counter App</h1>
-//         <button onClick={this.handleDecrement}>-</button>
-//         <span>
-//           {" "}
-//           {date.toDateString()} [{this.state.count}]
-//         </span>
-//         <button onClick={this.handleIncrement}>+</button>
-//       </div>
-//     );
-//   }
-// }
