500539633a7bb7a11bfe842c1e41d0030392dcc0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-21 03:22:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Welcome to Classy Weather App</h1>
-    </div>
-  );
-}
+import React, { Component } from "react";
 
-export default App;
+// export default class App extends Component {
+//   render() {
+//     return <div></div>;
+//   }
+// }
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleDecrement = () => {
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  };
+  handleIncrement = () => {
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
+  }
+}
