07fdec81ed2a8a80b5a5cb45cbb166184f8805b0
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-21 04:08:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import React, { Component } from "react";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, Classy Weather!</h1>
+      <div className="app">
+        <h1>Classy Weather</h1>
+
+        <div>
+          <input type="text" placeholder="Search for a city...." />
+        </div>
       </div>
     );
   }
