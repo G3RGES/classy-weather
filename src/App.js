9df1e6c78d06ca3cab31199a95771d5956abfc8b
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2026-01-21 04:33:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { location: "" };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  fetchWeather = () => {};
+
   render() {
     return (
       <div className="app">
         <h1>Classy Weather</h1>
 
         <div>
-          <input type="text" placeholder="Search for a city...." />
+          <input
+            type="text"
+            placeholder="Search for a city...."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
+
+        <button onClick={this.fetchWeather} style={{}}>
+          Get Weather
+        </button>
       </div>
     );
   }
